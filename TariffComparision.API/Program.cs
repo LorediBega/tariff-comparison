67da07c4f5634d4057aa9b500bf94505c8d8e3fe
Loredi <loredi.bega@gmail.com>
2024-12-09 17:32:39 +0100

    Enable annotations

--- TariffComparision.API/Program.cs
+++ TariffComparision.API/Program.cs
@@ -14,7 +14,10 @@ builder.Services.AddControllers(options =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.EnableAnnotations();
+});
 
 var app = builder.Build();
 
